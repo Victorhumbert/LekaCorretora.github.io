dc102f0826f79df685285fda5330615f34bc110c
patatiz <tormentavictor@gmail.com>
2023-03-08 14:25:01 -0300

    função de arrastar para o lado adicionado na galeria e imagens otimizadas

--- js/script.js
+++ js/script.js
@@ -4,3 +4,27 @@ var whatsappLink =
   "https://wa.me/5548998235646?text=Olá, Gostaria de mais informações sobre este imóvel: " +
   currentURL;
 document.getElementById("whatsapp-link").setAttribute("href", whatsappLink);
+
+// Galeria arrastar para o lado
+
+let gallery = document.querySelector(".galeria-imagens");
+let isDragging = false;
+let startX, scrollLeft;
+
+gallery.addEventListener("mousedown", (e) => {
+  isDragging = true;
+  startX = e.pageX - gallery.offsetLeft;
+  scrollLeft = gallery.scrollLeft;
+});
+
+gallery.addEventListener("mousemove", (e) => {
+  if (!isDragging) return;
+  e.preventDefault();
+  const x = e.pageX - gallery.offsetLeft;
+  const walk = (x - startX) * 1.5;
+  gallery.scrollLeft = scrollLeft - walk;
+});
+
+gallery.addEventListener("mouseup", () => {
+  isDragging = false;
+});
