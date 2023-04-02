e72681d01791d2288c8429e4ff3e9be026e657d6
patatiz <tormentavictor@gmail.com>
2023-04-02 10:55:07 -0300

    Adicionando mais casas

--- js/script.js
+++ js/script.js
@@ -5,26 +5,3 @@ var whatsappLink =
   currentURL;
 document.getElementById("whatsapp-link").setAttribute("href", whatsappLink);
 
-// Galeria arrastar para o lado
-
-let gallery = document.querySelector(".galeria-imagens");
-let isDragging = false;
-let startX, scrollLeft;
-
-gallery.addEventListener("mousedown", (e) => {
-  isDragging = true;
-  startX = e.pageX - gallery.offsetLeft;
-  scrollLeft = gallery.scrollLeft;
-});
-
-gallery.addEventListener("mousemove", (e) => {
-  if (!isDragging) return;
-  e.preventDefault();
-  const x = e.pageX - gallery.offsetLeft;
-  const walk = (x - startX) * 1.5;
-  gallery.scrollLeft = scrollLeft - walk;
-});
-
-gallery.addEventListener("mouseup", () => {
-  isDragging = false;
-});
